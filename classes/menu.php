6cfb971d31a9b620b6a6e080c0870525049157bd
René Hansen <renehh@gmail.com>
2012-06-24 19:53:54 +0200

    Added submenu for the notes page.

--- classes/menu.php
+++ classes/menu.php
@@ -22,13 +22,21 @@ class Menu
 
     add_submenu_page(
       'biblio_main',
-      'Test',
+      'Biblio - Test',
       'Test',
       'manage_options',
       'biblio_test',
       array($this, 'test_page')
     );
-    //add_submenu_page('biblio', 'I am reading', 'Amazon API', 'manage_options', 'biblio-amazon', 'iar_load_menu');
+    
+    add_submenu_page(
+      'biblio_main',
+      'Biblio - Notes',
+      'Notes',
+      'manage_options',
+      'biblio_notes',
+      array($this, 'notes_page')
+    );
   }
 
   public function main_page() {
@@ -37,6 +45,10 @@ class Menu
 
   public function test_page() {
     load_template(TEST_DIR . "database_handler_test.php");
+  }
+
+  public function notes_page() {
+    load_template(TEMPLATES_DIR . "notes.php");
   }
 }
 ?>
