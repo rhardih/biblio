55106d1cbeea7142d90969401dbbe2e888787588
René Hansen <renehh@gmail.com>
2012-07-09 23:38:01 +0200

    Defined markup of notes index page.

--- templates/notes.php
+++ templates/notes.php
@@ -1,20 +1,34 @@
 <?php
-
-//include(
-
-
-global $wpdb;
-$dh = new DatabaseHandler($wpdb, DB_TABLE_NAME);
-
+global $dh;
 ?>
 
-<pre>
-<?php $dh->current_readings(); ?>
-</pre>
-
 <div class="wrap">
-  <h2>Biblio</h2>
-  <h3>Current readings</h3>
+  <div id="biblio_wrapper">
+    <h2>Biblio</h2>
+    <h3>Current readings</h3>
 
-  <!-- Display a grid of currently started readings -->
+    <div id="readings" class="group">
+      <?php foreach ( $dh->current_readings() as $reading ) { ?>
+      <div class="reading">
+        <?php //echo var_dump($reading); ?>
+        <h4 class="title"><?php echo $reading->name ?></h4>
+        <div class="illustration">
+          <?php if($reading->url) { ?>
+          <img src="<?php echo $reading->url ?>" alt="Book" />
+          <?php } else { ?>
+          <div class="placeholder">
+            <img src="<?php echo BIBLIO_PLUGIN_URL . 'images/glyphicons_351_book_open.png' ?>" alt="Book" class="placeholder" />
+          </div>
+          <?php } ?>
+        </div>
+        <p>
+        <?php $_GET['book'] = $reading->id  ?>
+        <a href="?<?php echo http_build_query($_GET) ?>">Notes</a>
+        </p>
+      </div>
+      <?php } ?>
+    </div>
+
+    <h3>Past readings</h3>
+  </div>
 </div>
