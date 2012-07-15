2a5ff856e6ec8a5c04f290d9158a0bc954d7de97
René Hansen <renehh@gmail.com>
2012-07-15 11:59:39 +0200

    Added class DatabaseRessource.
    Attempt at creating a minimal ORM like abstraction.

---
+++ classes/database_resource.php
@@ -0,0 +1,91 @@
+<?php
+
+class DatabaseResource extends ArrayObject {
+
+  var $name;
+  var $id;
+
+  var $is_valid = false;
+
+  var $wpdb;
+
+  function __construct($id = null) {
+    global $wpdb;
+    $this->wpdb = $wpdb;
+
+    $this->name = $name;
+    if ($id) {
+      $this->id = $id;
+      $this->read();
+    }
+  }
+
+  public function create() {
+    foreach ($this as $attribute => $value) {
+      $attributes[] = $attribute;
+      $values[] = "'$value'";
+    }
+    $this->sql = sprintf(
+      'INSERT INTO %s (%s) VALUES (%s)',
+      DB_TABLE_NAME,
+      join(',', $attributes),
+      join(',', $values)
+    );
+  }
+
+  public function read() {
+    $sql = sprintf(
+      "SELECT * FROM %s WHERE id = %s;",
+      DB_TABLE_NAME,
+      $this->id
+    );
+    $read = $this->wpdb->get_row($sql);
+
+    error_log("SQL: $sql");
+    error_log(print_r($read, true));
+
+    if($read != null) {
+      foreach ($read as $attribute => $value) {
+        $this[$attribute] = $value;
+      }
+      $this->is_valid = true;
+    }
+  }
+
+  public function update($params) {
+    $tmp = array();
+    array_walk(
+      $params,
+      create_function(
+        '$val,$key,$result',
+        '$result[]="$key = \'$val\'";'
+      ),
+      &$tmp
+    );
+
+    $sql = sprintf(
+      "UPDATE %s SET %s WHERE id = %s;",
+      DB_TABLE_NAME,
+      join(', ', $tmp),
+      $this->id
+    );
+    error_log($sql);
+
+    return $res = $this->wpdb->update(
+      DB_TABLE_NAME,
+      $params,
+      array('id' => $this->id)
+    );
+  }
+
+  public function delete() {
+    $this->sql = sprintf(
+      "DELETE FROM %s WHERE id = %s;",
+      "whatever",
+      $this->id
+    );
+  }
+
+}
+
+?>
