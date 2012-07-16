0ce48eeab957ee5a9bd919b25b0b952e7cc8d450
René Hansen <renehh@gmail.com>
2012-07-16 23:19:24 +0200

    Added Read::all($status).
    Returns all reads from the database with set status.

--- classes/read.php
+++ classes/read.php
@@ -6,6 +6,27 @@ class Read extends DatabaseResource
     //{
     //  parent::__construct($id);
     //}
+
+  // Public: returns all readings of certain status
+  //
+  // $status - one of (Read::Active, Read::Inactive, Read::Done)
+  //
+  // Returns void.
+  public static function all($status) {
+
+    global $wpdb;
+
+    $sql = sprintf("SELECT * FROM %s WHERE status = %s;",
+      DB_TABLE_NAME,
+      $status
+    );
+
+    $reads = $wpdb->get_results($sql);
+    foreach($reads as $read) {
+      $reads[] = new Read($read);
+    }
+    return $reads;
+  }
 }
 
 ?>
