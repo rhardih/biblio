1e0bb06dc53d84eed067f757f27db09af02100df
René Hansen <renehh@gmail.com>
2012-07-19 00:13:52 +0200

    Added Current Readings Section.

--- templates/read.php
+++ templates/read.php
@@ -66,4 +66,41 @@ wp_nonce_field('add-read', 'add_wpnonce');
 submit_button('Submit');
 ?>
   </form>
+
+  <hr />
+
+  <?php $active_readings = Read::all(Read::Active); ?>
+
+  <h3>Current readings</h3>
+  <div class="readings_wrapper">
+    <div id="readings" class="group">
+      <?php foreach ( $active_readings as $reading ) { ?>
+      <div class="reading_wrapper">
+        <div class="reading">
+          <div class="title_author_wrapper">
+            <h4 class="title"><?php echo $reading->title; ?></h4>
+            <p class="author"><?php echo $reading->author; ?></p>
+          </div>
+          <div class="illustration">
+            <?php if($reading->url) { ?>
+            <img src="<?php echo $reading->url ?>" alt="Book" />
+            <?php } else { ?>
+            <div class="placeholder">
+              <img src="<?php echo BIBLIO_PLUGIN_URL . 'images/glyphicons_351_book_open.png' ?>" alt="Book" class="placeholder" />
+            </div>
+            <?php } ?>
+          </div>
+          <p class="actions">
+          <a href="<?php echo http_build_query($_GET) ?>">Finish</a>
+          <a href="<?php echo http_build_query($_GET) ?>">Pause</a>
+          <a href="<?php echo http_build_query($_GET) ?>">Notes</a>
+          <a href="<?php echo http_build_query($_GET) ?>">Delete</a>
+          </p>
+        </div>
+      </div>
+      <?php } ?>
+    </div>
+  </div>
+  <h3>Past readings</h3>
 </div>
+
