d7be269478770b3bd73eeb60d5760fc83f94e701
René Hansen <renehh@gmail.com>
2012-07-23 01:10:25 +0200

    Rename $reading to $read.

--- templates/read/_action_button.php
+++ templates/read/_action_button.php
@@ -1,6 +1,6 @@
 <form class="<?php echo $action; ?>" action="?page=biblio_main" method="post">
   <input type="hidden" name="action" value="<?php echo $action; ?>" />
-  <input type="hidden" name="id" value="<?php echo $reading['id']; ?>" />
+  <input type="hidden" name="id" value="<?php echo $read['id']; ?>" />
   <input type="hidden" name="status" value="<?php echo $status_value; ?>" />
   <?php wp_nonce_field('read', 'biblio_nonce'); ?>
   <?php submit_button($text, 'secondary', 'submit', false) ?>
