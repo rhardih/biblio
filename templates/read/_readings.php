d37a0e427301eff71438396db2820e0116bb034d
René Hansen <renehh@gmail.com>
2012-07-23 01:09:17 +0200

    Renamed $reading to $read.

--- templates/read/_readings.php
+++ templates/read/_readings.php
@@ -1,15 +1,15 @@
 <div class="readings_wrapper">
   <div id="readings" class="group">
-    <?php foreach ( $readings as $reading ) { ?>
+    <?php foreach ( $readings as $read ) { ?>
     <div class="reading_wrapper">
       <div class="reading">
         <div class="title_author_wrapper">
-          <h4 class="title"><?php echo $reading['title']; ?></h4>
-          <p class="author"><?php echo $reading['author']; ?></p>
+          <h4 class="title"><?php echo $read['title']; ?></h4>
+          <p class="author"><?php echo $read['author']; ?></p>
         </div>
         <div class="illustration">
-          <?php if($reading['url']) { ?>
-          <img src="<?php echo $reading['url'] ?>" alt="Book" />
+          <?php if($read['url']) { ?>
+          <img src="<?php echo $read['url'] ?>" alt="Book" />
           <?php } else { ?>
           <div class="placeholder">
             <img src="<?php echo BIBLIO_PLUGIN_URL . 'images/glyphicons_351_book_open.png' ?>" alt="Book" class="placeholder" />
@@ -18,7 +18,7 @@
         </div>
         <div class="actions">
           <?php
-          switch ($reading['status']) {
+          switch ($read['status']) {
           case Read::Begun:
             $action = 'update';
             $status_value = Read::Done;
