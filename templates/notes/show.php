609de81026b43c58e896e2902febe560aa0a5486
René Hansen <renehh@gmail.com>
2012-07-23 01:08:20 +0200

    Cleanup and alignment with read.php.

--- templates/notes/show.php
+++ templates/notes/show.php
@@ -1,10 +1,6 @@
 <?php
-
-if (isset($_GET['read'])) {
-  
-  $read = new Read($_GET['read']);
-
-  if ($read->is_valid) {
+$read = new Read(intval($_GET['read_id']));
+if ($read->is_valid) {
 ?>
 
 <h3>Notes</h3>
@@ -18,22 +14,14 @@ if (isset($_GET['read'])) {
   </div>
   <input type="hidden" name="read" value="<?php echo $_GET['read']; ?>" />
 
-<?php
-wp_nonce_field('biblio-add','name_of_nonce_field');
-submit_button("Submit");
-?>
-
+  <?php
+  wp_nonce_field('biblio-add','name_of_nonce_field');
+  submit_button("Submit");
+  ?>
 </form>
 
 <?php
   } else {
-?>
-
-<div class="error">
-  <p>No Read by that id.</p>
-</div>
-
-<?php
+    error('No Read by that id.');
   }
-}
 ?>
