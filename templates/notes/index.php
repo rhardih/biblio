22af9799e91a6661587afd3cc8b48ab128a3f59b
René Hansen <renehh@gmail.com>
2012-07-23 01:07:23 +0200

    Make use of _readings partial.

--- templates/notes/index.php
+++ templates/notes/index.php
@@ -1,32 +1,21 @@
+<h3>Current readings</h3>
 <?php
-global $dh;
+$readings = Read::all(Read::Begun);
+
+if (count($readings) > 0) {
+  include('_readings.php');
+} else {
+  echo 'No current readings.';
+}
 ?>
 
-<h3>Current readings</h3>
-
-<div id="readings" class="group">
-  <?php foreach ( $dh->current_readings() as $reading ) { ?>
-  <div class="reading">
-    <?php //echo var_dump($reading); ?>
-    <h4 class="title"><?php echo $reading->name ?></h4>
-    <div class="illustration">
-      <?php if($reading->url) { ?>
-      <img src="<?php echo $reading->url ?>" alt="Book" />
-      <?php } else { ?>
-      <div class="placeholder">
-        <img src="<?php echo BIBLIO_PLUGIN_URL . 'images/glyphicons_351_book_open.png' ?>" alt="Book" class="placeholder" />
-      </div>
-      <?php } ?>
-    </div>
-    <p>
-    <?php
-      $_GET['read'] = $reading->id;
-      $_GET['action'] = 'show';
-    ?>
-    <a href="?<?php echo http_build_query($_GET) ?>">Notes</a>
-    </p>
-  </div>
-  <?php } ?>
-</div>
-
 <h3>Past readings</h3>
+
+<?php
+$readings = Read::all(Read::Done);
+if (count($readings) > 0) {
+  include('_readings.php');
+} else {
+  echo 'No current readings.';
+}
+?>
