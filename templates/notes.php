1e648c4c3e9f11423066fe2480582f4a8fa8b030
René Hansen <renehh@gmail.com>
2012-07-23 01:06:40 +0200

    Indentation. Use of get_admin_page_title().

--- templates/notes.php
+++ templates/notes.php
@@ -1,6 +1,6 @@
 <?php
 
-var_dump($_POST);
+echo '<pre>' . print_r($_POST, true) . '</pre>';
 
 if ($_POST) {
   $read = new Read($_POST['read']);
@@ -23,25 +23,24 @@ if ($_POST) {
 }
 ?>
 
-<div class="wrap">
-  <div id="biblio_wrapper">
-    <h2>Biblio</h2>
+<div class="wrap biblio">
+  <h2><?php echo get_admin_page_title(); ?></h2>
+  <hr />
 
-<?php
-switch ($_GET['action']) {
-case "edit":
-  include(TEMPLATES_DIR . 'notes/edit.php');
-  break;
-case "new":
-  include(TEMPLATES_DIR . 'notes/new.php');
-  break;
-case "show":
-  include(TEMPLATES_DIR . 'notes/show.php');
-  break;
-default:
-  include(TEMPLATES_DIR . 'notes/index.php');
-}
-?>
+  <?php
+  switch ($_GET['action']) {
+  case "edit":
+    include(TEMPLATES_DIR . 'notes/edit.php');
+    break;
+  case "new":
+    include(TEMPLATES_DIR . 'notes/new.php');
+    break;
+  case "show":
+    include(TEMPLATES_DIR . 'notes/show.php');
+    break;
+  default:
+    include(TEMPLATES_DIR . 'notes/index.php');
+  }
+  ?>
 
-  </div>
 </div>
