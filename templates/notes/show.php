998dd7019658c290d9be2a1ce665ea2ae95a4e42
René Hansen <renehh@gmail.com>
2012-07-23 20:09:01 +0200

    Updated notes form.

--- templates/notes/show.php
+++ templates/notes/show.php
@@ -6,16 +6,21 @@ if ($read->is_valid) {
 <h3><?php echo $read['title'] ?> - <?php echo $read['author'] ?></h3>
 <br />
 
-<form method="post" action="?page=biblio_notes">
+<form method="post" action="<?php echo biblio_url($_GET); ?>">
   <div id="poststuff">
     <?php the_editor($read['notes']); ?>
   </div>
-  <input type="hidden" name="read" value="<?php echo $_GET['read']; ?>" />
+  <input type="hidden" name="action" value="update" />
+  <input type="hidden" name="read_id" value="<?php echo $_GET['read_id']; ?>" />
 
-  <?php
-  wp_nonce_field('biblio-add','name_of_nonce_field');
-  submit_button("Submit");
-  ?>
+  <p class="submit">
+    <?php
+    wp_nonce_field('note','biblio_nonce');
+    submit_button("Save note", 'primary', 'submit', false);
+    $params = array_intersect_key($_GET, array('page' => 0));
+    ?>
+    or <a href="<?php echo biblio_url($params); ?>">Back</a>
+  </p>
 </form>
 
 <?php
