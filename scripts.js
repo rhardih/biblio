45ca1bf009b121fb5a2d6c65f8f7796031fb96ad
René Hansen <renehh@gmail.com>
2012-07-23 22:48:11 +0200

    Added js to handle image upload dialog.

--- scripts.js
+++ scripts.js
@@ -11,5 +11,18 @@
 
       return confirm(msg);
     });
+
+    $('#illustration_button').click(function() {
+      formfield = $('#illustration').attr('name');
+      tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
+      return false;
+    });
+
+    window.send_to_editor = function(html) {
+      imgurl = $('img',html).attr('src');
+      $('#illustration').val(imgurl);
+      tb_remove();
+    }
+
   });
 })(jQuery);
